a4a1b2850725600b378dce9168886103a7e46853
Vahide Segah Üney <vahidesegah@gmail.com>
2025-01-27 18:35:01 +0300

    bıgıbıgı

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-js-hw-11">
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
