94e26b605ffdef584133e5f65a93f1b706d7be97
Vahide Segah Üney <vahidesegah@gmail.com>
2025-01-27 19:38:51 +0300

    css

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -98,6 +98,7 @@ const ImageGallery = () => {
             required
           />
           <button
+            class="buton"
             type="submit"
             className="px-6 py-2 bg-blue-500 text-white rounded"
           >
